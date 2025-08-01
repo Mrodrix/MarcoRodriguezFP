419fdb29a682cfbdf22f8b0510309ebf3cec697a
Mrodrix <markolino3053@gmail.com>
2025-08-01 15:23:48 -0500

    Variables, constantes y tipos de datos

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -4,8 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("HOLA, Marco\n");
+            //Esta es la declaración e inicialización de una variable
+            string nombre = "Marco";
+            const string iva="19%";
+            Console.Write("HOLA,"+nombre+" \n");
             Console.Write("\tBienvenido a la clase de programacion");
+            nombre = "Alejandro";
+            Console.Write("\n" + nombre);
+            //iva = "20%"; No se puede cambiar el valor de una constante
+
+            //Tipos de datos
+            byte dato1 = 255;
+            int dato2 = 20542;
+            long dato3 = 912491824;
+            double dato4 = 352.65;
+            char dato5 = 'a';//Puede almacenar solo un caracter
+            bool dato6 = false;//Almacena true o false
+            object dato7 = new Program();
         }
     }
 }
