6494433bc092ae82aa91d911d6a6cf392d33bc74
Marco Rodriguez <markolino3053@gmail.com>
2025-08-09 12:33:16 -0500

    condicionales inicio

--- 3.Condicionales/3.Condicionales/Program.cs
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           /*Crear un algoritmo que permita ingresar el nombre y 
+            el sueldo de una persona, si el sueldo supera los 3000 
+            pesos mostrar el mensaje en pantalla con el nombre de la persona,
+            indicando que debe abonar impuestos.*/
+
+            //Condicional simple
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine(); //Todo lo que leo desde la consola llega como string
+            Console.WriteLine(nombre);
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo>3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+
+
+            /*Crea un algoritmo que lea la edad de un usuario,
+            si el usuario es mayor de 18, se debe mostrar el siguiente mensaje:
+            “Bienvenido a mi sitio web”.*/
         }
     }
 }
