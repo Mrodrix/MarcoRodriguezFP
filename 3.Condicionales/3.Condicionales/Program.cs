1823275bb6aa020abca545d32d61c822829140c1
Marco Rodriguez <markolino3053@gmail.com>
2025-08-09 11:45:28 -0500

    condicionales y cambio

---
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
