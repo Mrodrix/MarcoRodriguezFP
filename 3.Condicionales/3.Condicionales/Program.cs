9ba9d603265be1b612d750143797b1979b579bba
Marco Rodriguez <markolino3053@gmail.com>
2025-08-13 14:34:56 -0500

    cambio

--- 3.Condicionales/3.Condicionales/Program.cs
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -7,7 +7,7 @@
             /*Crear un algoritmo que permita ingresar el nombre y 
              el sueldo de una persona, si el sueldo supera los 3000 
              pesos mostrar el mensaje en pantalla con el nombre de la persona,
-             indicando que debe abonar impuestos.
+             indicando que debe abonar impuestos.*/
 
              //Condicional simple
              string nombre = "";
@@ -22,30 +22,8 @@
              if (sueldo>3000)
              {
                  Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
-             }*/
-
-
-            /*Crea un algoritmo que lea la edad de un usuario,
-            si el usuario es mayor de 18, se debe mostrar el siguiente mensaje:
-            “Bienvenido a mi sitio web”.*/
-            
-            
-            /*Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, 
-            se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”.*/
-
-            int edad = 0;
-
-            Console.WriteLine("Por Favor Ingrese Su Edad");
-            edad = Convert.ToInt32(Console.ReadLine());
-
-            if (edad >= 18)
-            {
-                Console.WriteLine("Bienvenido a mi sitio web");
-            }
-
-            
-
-
+             }
+        
         }
     }
 }
