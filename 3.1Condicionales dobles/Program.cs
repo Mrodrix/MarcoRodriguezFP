240fce8fe9bb5ead7c35a835ccb6e7893e488070
Marco Rodriguez <markolino3053@gmail.com>
2025-08-13 15:00:54 -0500

    tarea cambio

---
+++ 3.1Condicionales dobles/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _3._1Condicionales_dobles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona,
+            si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona,
+            indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”. */
+
+
+
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if (sueldo >= 3000)
+            {
+                //verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Falsa
+                Console.WriteLine($"La persona {nombre}, no abona impuestos");
+            }
+        }
+    }
+}
