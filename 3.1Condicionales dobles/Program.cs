4da4608665748aa19833636b287df4f11a538208
Marco Rodriguez <markolino3053@gmail.com>
2025-08-13 15:05:44 -0500

    ejercicio

--- 3.1Condicionales dobles/Program.cs
+++ 3.1Condicionales dobles/Program.cs
@@ -7,7 +7,7 @@
 
             /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona,
             si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona,
-            indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”. */
+            indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”. 
 
 
 
@@ -28,8 +28,30 @@
             else
             {
                 //Falsa
-                Console.WriteLine($"La persona {nombre}, no abona impuestos");
+                Console.WriteLine($"La persona {nombre}, no abona impuestos");*/
+
+            /*Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18,
+            se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. De lo contrario,
+            mostrar el mensaje “No es apto para el contenido de este sitio web”.  */
+                      
+            int edad = 0;
+
+            Console.WriteLine("Ingrese Su Edad");
+
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+
+                Console.WriteLine("Bienvenido a mi sitio web");
+
             }
+            else
+            {
+                Console.WriteLine("No es apto para este sitio web");
+            }
+
         }
     }
+    
 }
