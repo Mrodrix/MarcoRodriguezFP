826bfb8e8ed458e9c26907f747f635d78b6e7acb
Mrodrix <markolino3053@gmail.com>
2025-08-29 15:30:13 -0500

    Add files via upload
    no se la verdad

---
+++ 10. while 2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _10._while_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un numero dado por el usuario
+
+            int numero = 0;
+            int acumulador = 1;
+            int contador = 1;
+
+            Console.WriteLine("Ingrese un número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                acumulador = acumulador * contador;
+                Console.WriteLine($"Contador: {contador} - Factorial: {acumulador}");
+                contador++;
+            }
+
+
+            Console.WriteLine($"El factorial de {numero} es {acumulador}");
+        }
+    }
+}
---
+++ 9.ciclo while/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _9.ciclo_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int acumulador = 0;
+            
+            while(contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"C: {contador} - A:{acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"La suma de los numeros es: {acumulador}");
+        }
+    }
+}
