96bfe94477e07fc955eee27153c01bb3913cb27c
Mrodrix <markolino3053@gmail.com>
2025-08-30 09:43:23 -0500

    Add files via upload
    ejercicios while

---
+++ 11. while ejercicio/11. while ejercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _11._while_ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*11. Realizar un algoritmo que pida números enteros positivos y los sume,
+             hasta que se ingrese un número entero negativo.
+             Se debe mostrar por pantalla el total de la suma de los números ingresados. */
+
+            int numero;
+            int contador = 0;
+
+            Console.WriteLine("Ingrese números enteros positivos. Para salir, ingrese un número negativo.");
+
+            Console.Write("Ingrese un número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                contador= contador+numero;
+
+                Console.WriteLine($"el numero es {contador}");
+                Console.Write("Ingrese otro número: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma total de los números positivos ingresados es: " + contador);
+        }
+    }
+}
---
+++ 12. ejercicio impares while/12. ejercicio impares while/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _12._ejercicio_impares_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*12. Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99. */
+            
+            int numero = 1;
+            int contador = 0;
+
+            while (numero <= 99)
+            {
+                contador = contador + numero;
+                numero = numero + 2; 
+            }
+
+            Console.WriteLine("La suma de los números impares hasta 99 es: " + contador);
+        }
+    }
+}
