7abc973108e844c426dc443bc7309c5d46b863e6
Mrodrix <markolino3053@gmail.com>
2025-09-10 19:59:29 -0500

    Add files via upload
    ejercicios do while

---
+++ 19. ejercicio do while 3/19. ejercicio do while 3/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic;
+
+namespace _19._ejercicio_do_while_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*19. En un grupo de 100 alumnos, se desea saber cuántos de los estudiantes son hombres y cuántos son mujeres,
+            además se desea saber cuántos son mayores de edad y cuántos no.  */
+
+            int cantidadEstudiantes = 0;
+            int edad = 0;
+            int genero = 0;
+            int cantidadHombres = 0;
+            int CantidadMujeres = 0;
+            int cantidadmayores = 0;
+            int cantidadmenores = 0;
+            
+
+            do
+            {
+                Console.WriteLine($"Estudiante {cantidadEstudiantes}:");
+                Console.WriteLine("Ingrese su edad");
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese su genero: 1.Hombre 2.Mujer");
+                genero = int.Parse(Console.ReadLine());
+
+                if (genero == 1)
+                {
+                    cantidadHombres++;
+                }
+                else
+                {
+                    CantidadMujeres++;
+                }
+                if (edad >= 18)
+                {
+                    cantidadmayores++;
+                }
+                else
+                {
+                    cantidadmenores++;
+                }
+                cantidadEstudiantes++;
+            } while (cantidadEstudiantes <= 100);
+
+            Console.WriteLine($"De los 100 estudiantes, {cantidadHombres} son hombres, {CantidadMujeres} son mujeres, {cantidadmenores} son menores y {cantidadmayores} son mayores");
+        }
+    }
+}
---
+++ 20. ejercicio do while 4/20. ejercicio do while 4/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _20._ejercicio_do_while_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*20. Un equipo de futbol jugó 30 partidos en el año, se desea crear un algoritmo que permita saber:   
+            El número de partidos perdidos  
+            El número de partidos empatados  
+            El número de partidos ganados  
+            El porcentaje de partidos perdidos  
+            El porcentaje de partidos empatados  
+            El porcentaje de partidos ganados */
+
+            int contador = 1;
+            int ganados = 0;
+            int perdidos = 0;
+            int empatados = 0;
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("Partido #" + contador);
+                Console.WriteLine("Ingrese el resultado del partido: 1. Ganado  2. Perdido  3. Empatado");
+                Console.Write("Opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                if (opcion == 1)
+                {
+                    ganados++;
+                }
+                else if (opcion == 2)
+                {
+                    perdidos++;
+                }
+                else if (opcion == 3)
+                {
+                    empatados++;
+                }
+                
+
+                contador++;
+
+            } while (contador <= 30);
+
+            double porcentajeGanados = (ganados * 100.0) / 30;
+            double porcentajePerdidos = (perdidos * 100.0) / 30;
+            double porcentajeEmpatados = (empatados * 100.0) / 30;
+
+            Console.WriteLine("\nResultados del equipo en 30 partidos:");
+            Console.WriteLine("Partidos ganados: " + ganados);
+            Console.WriteLine("Partidos perdidos: " + perdidos);
+            Console.WriteLine("Partidos empatados: " + empatados);
+            Console.WriteLine("Porcentaje de partidos ganados: " + porcentajeGanados + "%");
+            Console.WriteLine("Porcentaje de partidos perdidos: " + porcentajePerdidos + "%");
+            Console.WriteLine("Porcentaje de partidos empatados: " + porcentajeEmpatados + "%");
+        }
+    }
+}
