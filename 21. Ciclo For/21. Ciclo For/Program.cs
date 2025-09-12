f2fae75e92233dd20905298c9d989a99bca09a60
Mrodrix <markolino3053@gmail.com>
2025-09-12 14:44:54 -0500

    Add files via upload

---
+++ 21. Ciclo For/21. Ciclo For/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _21._Ciclo_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                acumulador += i;
+            }
+            Console.WriteLine($"la suma de los cinco primeros números enteros es {acumulador}");
+        }
+    }
+}
