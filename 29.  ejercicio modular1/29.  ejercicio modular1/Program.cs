8a7303a0d011d05587d2cf30ab5376b4762b1b2b
Mrodrix <markolino3053@gmail.com>
2025-10-15 14:15:52 -0500

    Add files via upload

---
+++ 29.  ejercicio modular1/29.  ejercicio modular1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _29.__ejercicio_modular1
+{
+    internal class Program
+    {
+           /* Crear una matriz de n filas por m columnas, que permita ingresar solamente 
+           múltiplos de 5 hasta que se llene la matriz.*/
+        static void Main(string[] args)
+        {
+            int[,] multiplosDe5 = new int[CapturarFilas(), CapturarColumnas()];
+        }
+        static int CapturarFilas()
+        {
+            Console.WriteLine("Ingrese el número de filas para la matriz:");
+            int filas = int.Parse(Console.ReadLine());
+            return filas;
+        }
+
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese el número de columnas para la matriz:");
+            int columnas = int.Parse(Console.ReadLine());
+            return columnas;
+        }
+
+        static int[,] LlenasMatriz(int[,] matriz)
+        {
+            return matriz;
+        }
+
+    }
+}
